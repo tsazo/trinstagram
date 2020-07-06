1b67a5ca53e0a882c14c255989cf0854939e1914
tsazo <trinitysazo@gmail.com>
2020-07-06 13:14:39 -0700

    Added a login activity and designed the XML file along with the activity.

---
+++ app/src/main/java/com/codepath/tsazo/trinstagram/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.codepath.tsazo.trinstagram;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * A login screen that offers login via username/password
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Login button clicked");
+                String username = editTextUsername.getText().toString();
+                String password = editTextPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user: " + username);
+        //TODO: navigate to main activity if the user has
+    }
+}
