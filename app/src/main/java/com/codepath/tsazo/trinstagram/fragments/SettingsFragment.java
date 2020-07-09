591a6eb870336316d8ba8e328f9d1e3847ee8d98
tsazo <trinitysazo@gmail.com>
2020-07-09 14:50:26 -0700

    Added update username and password functionality under the settings page.

--- app/src/main/java/com/codepath/tsazo/trinstagram/fragments/SettingsFragment.java
+++ app/src/main/java/com/codepath/tsazo/trinstagram/fragments/SettingsFragment.java
@@ -10,18 +10,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.codepath.tsazo.trinstagram.activities.LoginActivity;
 import com.codepath.tsazo.trinstagram.R;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends Fragment {
 
-    public static final String TAG = "PostsFragment";
+    public static final String TAG = "SettingsFragment";
     private Button buttonLogout;
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private EditText editTextEmail;
+    private ImageView imageViewProfile;
+    private ParseUser currentUser;
+    private Button buttonChangeProfile;
+
+    private static final String KEY_PROFILE_PIC = "profilePicture";
 
 
     public SettingsFragment() {
@@ -46,8 +63,50 @@ public class SettingsFragment extends Fragment {
 
         // Setup any handles to view objects here
         buttonLogout = view.findViewById(R.id.buttonLogout);
+        editTextUsername = view.findViewById(R.id.editTextUsername);
+        editTextPassword = view.findViewById(R.id.editTextPassword);
+        editTextEmail = view.findViewById(R.id.editTextEmail);
+        imageViewProfile = view.findViewById(R.id.imageViewProfile);
+        buttonChangeProfile = view.findViewById(R.id.buttonChangeProfile);
 
-        // On take picture, open camera (or even camera roll)
+        // Gets the person who's logged in
+        currentUser = ParseUser.getCurrentUser();
+
+        // Set values
+        setValues();
+
+        // Update profile information
+        updateProfile();
+
+        // Logout button listener
+        logout();
+    }
+
+    // Set listener to update profile
+    private void updateProfile() {
+        buttonChangeProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentUser.setUsername(editTextUsername.getText().toString());
+                currentUser.setEmail(editTextEmail.getText().toString());
+
+                currentUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e != null){
+                            Log.e(TAG, "Error while saving", e);
+                            Toast.makeText(getContext(), "Error updating profile!", Toast.LENGTH_SHORT).show();
+                        }
+                        Log.i(TAG, "update profile save was successful!");
+                        Toast.makeText(getContext(), "Updated profile!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    // Logout button listener
+    private void logout() {
         buttonLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -59,6 +118,25 @@ public class SettingsFragment extends Fragment {
                 goLoginActivity();
             }
         });
+    }
+
+    private void setValues() {
+        editTextUsername.setText(currentUser.getUsername());
+        editTextEmail.setText(currentUser.getEmail());
+        ParseFile image = currentUser.getParseFile(KEY_PROFILE_PIC);
+
+        if(image != null) {
+            // Binds image to ViewHolder with rounded corners
+            int radius = 360; // corner radius, higher value = more rounded
+            int margin = 0; // crop margin, set to 0 for corners with no crop
+            Log.i(TAG, String.valueOf(currentUser.getParseFile(KEY_PROFILE_PIC).getUrl()));
+
+            Glide.with(getContext())
+                    .load(currentUser.getParseFile(KEY_PROFILE_PIC).getUrl())
+                    .fitCenter()
+                    .transform(new RoundedCornersTransformation(radius, margin))
+                    .into(imageViewProfile);
+        }
     }
 
     // Goes to LoginActivity on click
